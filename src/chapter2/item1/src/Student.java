bdaca741426678b9339c9ac5efbb50d346ee39f6
Hyun-juhee <juhe1998@naver.com>
2021-08-13 12:48:37 +0900

    feat(item1) : 정적 팩토리 메소드 실습

---
+++ src/chapter2/item1/src/Student.java
@@ -0,0 +1,27 @@
+package chapter2.item1.src;
+
+public class Student {
+    private String name;
+    private static final Student instance = new Student("singleton");
+
+    private Student(String name) {
+        this.name = name;
+    }
+    private Student() {
+    }
+
+    public static Student from(String name) {
+        return new Student(name);
+    }
+
+    public static Student getInstance() {
+        return instance;
+    }
+
+    public static Student getStudent(boolean flag) {
+        return flag ? new PrizeStudent() : new Student();
+    }
+
+    static class PrizeStudent extends Student {
+    }
+}
